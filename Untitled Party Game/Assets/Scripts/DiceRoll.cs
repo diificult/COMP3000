1d7aaa61353356260e3096759a2e50f5e0c5511b
Owen <owenhowarth@gmail.com>
2020-10-30 15:42:53 +0000

    Dice Roll Change
    Moved dice roll code into DICE UI.
    
    Created first map position

---
+++ Untitled Party Game/Assets/Scripts/DiceRoll.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Random = UnityEngine.Random;
+
+public class DiceRoll : MonoBehaviour
+{
+
+    private bool DiceRolled = false;
+    public void TurnStart ()
+    {
+        DiceRolled = false;
+    }
+
+    void Update()
+    {
+        if (!DiceRolled)
+        {
+            GenerateNumber();
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                DiceRolled = true;
+            }
+        }else
+        {
+            
+        }
+
+    }
+
+    private void GenerateNumber()
+    {
+        int randomNumber = Random.Range(1, 7);
+        GetComponent<Text>().text = randomNumber.ToString();
+    }
+}
+
