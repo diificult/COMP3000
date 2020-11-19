de0986ee91e923dcbc6470ebb14dbb1ae3ade523
Owen H <owenhowarth@gmail.com>
2020-11-19 23:32:45 +0000

    Fixed Project, URP and Cinemachine
     The project was having issues, reinstalled unity on my machine and all issues "SHOULD" be fixed with packages not working and broken textures.
    
    This required creating a new project, but it gave me a chance to use the universal render pipeline to setup the project.
    
    Cinemachine now works as I intended it to with it following the player and now switches between players.

---
+++ UPG/Assets/Scripts/DiceRoll.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Random = UnityEngine.Random;
+using UnityEngine.Events;
+
+
+
+public class DiceRoll : MonoBehaviour
+{
+    private bool diceRolled = false;
+    private int Value;
+
+    void Update()
+    {
+        if (!diceRolled) GenerateNumber();    
+    }
+
+    public void NewTurn()
+    {
+        GetComponent<Text>().enabled = true;
+        diceRolled = false;
+    }
+
+    public void DiceRolled(int v)
+    {
+        diceRolled = true;
+        Value = v;
+        GetComponent<Text>().text = Value.ToString();
+    }
+
+    private void GenerateNumber()
+    {
+        GetComponent<Text>().text = Random.Range(1, 7).ToString();
+    }
+
+
+    public void ChangeValue (int i)
+    {
+        Value = i;
+        GetComponent<Text>().text = Value.ToString();
+    }
+
+
+    public void HideDice()
+    {
+        GetComponent<Text>().enabled = false;
+    }
+
+
+}
+
---
+++ UPG/Assets/Scripts/SpotPointers.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpotPointers : MonoBehaviour
+{
+
+    public bool SplitSpot = false;
+
+    public GameObject[] nextSpot = new GameObject[1];
+
+    public GameObject[] prevSpot = new GameObject[1];
+
+}
