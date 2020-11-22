c095a5dd2e555e5e75a1df48162da08aa988d159
Owen H <owenhowarth@gmail.com>
2020-11-22 18:02:34 +0000

    New map design and Spot Method
    Spots are now one prefab. Replaced old spots with new ones

--- UPG/Assets/Scripts/SpotPointers.cs
+++ UPG/Assets/Scripts/SpotPointers.cs
@@ -7,10 +7,24 @@ public class SpotPointers : MonoBehaviour
 
     public bool SplitSpot = false;
 
+    public int SpotType = 0;
+    // 0 = Default
+    // 1 = Green
+    // 2 = Red
+    // 3 = Blue
+    // 4 = Gold
+
+    public Material[] SpotMaterials;
+
     public GameObject[] nextSpot = new GameObject[1];
 
     public GameObject[] prevSpot = new GameObject[1];
 
     public GameObject[] splitArrows;
 
+
+     void Start()
+    {
+        gameObject.transform.GetChild(0).gameObject.GetComponent<MeshRenderer>().material = SpotMaterials[SpotType];
+    }
 }
